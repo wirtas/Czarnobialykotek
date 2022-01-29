13717a68698910230ac96f6e9cbd2c1481d045d4
Wirtas <wirtekkr@gmail.com>
2022-01-30 00:51:33 +0100

    Fixed Walls part 420

--- Assets/Scripts/TicketInspector.cs
+++ Assets/Scripts/TicketInspector.cs
@@ -6,7 +6,6 @@ public class TicketInspector : MonoBehaviour
 {
     [SerializeField] private bool isLeftWall = true;
     [SerializeField] private Collider2D wall;
-    [SerializeField] private GameObject[] oppositeWalls;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (!col.CompareTag("Player")) return;
@@ -15,10 +14,6 @@ public class TicketInspector : MonoBehaviour
         if ((!playerColor.IsRightBlack || !isLeftWall) && (playerColor.IsRightBlack || isLeftWall)) return;
         
         wall.gameObject.SetActive(false);
-        foreach (GameObject oppositeWall in oppositeWalls)
-        {
-            oppositeWall.SetActive(false);
-        }
     }
 
     public void PlayerLeft()
